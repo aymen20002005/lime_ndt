13e511523e9fe6a548b4b7507d74737a36f85239
Aymenn <mimobouyahia@gmail.com>
2025-08-06 19:38:49 +0100

    update version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lime_ndt",
-    version="0.1.0",
+    version="0.1.1",
     author="Mohamed Aymen BOUYAHIA",
     author_email="mohamed-aymen.bouyahia@ensta-paris.fr",
     description="Enhancing LIME through Neural Decision Trees",
