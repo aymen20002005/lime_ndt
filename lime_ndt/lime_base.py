86db9a5560a5ca6c83fd993d3e5a081d5df3bae8
Aymenn <mimobouyahia@gmail.com>
2025-08-09 17:15:08 +0100

    code refactor

--- lime_ndt/lime_base.py
+++ lime_ndt/lime_base.py
@@ -3,7 +3,6 @@ Contains abstract functionality for learning locally linear sparse model.
 """
 import numpy as np
 import scipy as sp
-#from lime_ndt import RTF
 from sklearn.linear_model import Ridge, lars_path
 from sklearn.utils import check_random_state
 
